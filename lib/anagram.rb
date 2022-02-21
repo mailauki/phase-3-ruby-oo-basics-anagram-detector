9838385e5a86187091dfaec9048c5d7d4c9e77ea
Julie Evans <juliemevans17@gmail.com>
2022-02-20 21:11:45 -0600

    commit tree 03dd2011ad7d61f2119148e24c8e82208a441a3b

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,11 @@
-# Your code goes here!
+class Anagram
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array = %w[enlists google inlets banana])
+    array.filter { |item| item.chars.sort == word.chars.sort ? item : nil }
+  end
+end
